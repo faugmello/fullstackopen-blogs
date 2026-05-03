b5ea80c360b14ead15a6bd570c6ff405cf76932e
Fabiano Augusto de Mello <faugmello@proton.me>
2026-05-03 18:01:21 -0300

    exercise 4.4 answer

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,4 +2,10 @@ const dummy = (blogs) => {
     return 1
 }
 
-module.exports = { dummy }
+const totalLikes = (blogs) => {
+    return blogs.length === 0
+        ? 0
+        : blogs.reduce((sum, blog) => sum + blog.likes, 0)
+}
+
+module.exports = { dummy, totalLikes }
